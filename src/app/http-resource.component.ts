bb1f97fc74e0c0df6446527792e62c876c313e56
Michael Small <chickenhat22@gmail.com>
2025-02-14 20:38:19 -0600

    chore: type resource and give default value

--- src/app/http-resource.component.ts
+++ src/app/http-resource.component.ts
@@ -1,6 +1,7 @@
 import { JsonPipe } from '@angular/common';
 import { httpResource } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Todo } from './todo.service';
 
 @Component({
   selector: 'app-http-resource',
@@ -15,7 +16,7 @@ export class HttpResourceComponent {
 
     // For more examples, see the tests from the PR: 
     // https://github.com/angular/angular/pull/59876/files#diff-a3ccb3778a1821be2d303f932f53036a5ff507cda412db0ed800fa55dba698bdR14
-    
+
     // Default GET
-    todos = httpResource(this.url)
+    todos = httpResource<Todo[]>(this.url, {defaultValue: []})
 }
