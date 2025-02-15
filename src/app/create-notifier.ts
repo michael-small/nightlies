5f26ff94911b922cda4e798fa378ce7092623b5b
Michael Small <chickenhat22@gmail.com>
2025-02-14 21:18:57 -0600

    chore: `POST` example of `httpResource`

---
+++ src/app/create-notifier.ts
@@ -0,0 +1,13 @@
+import { signal } from "@angular/core";
+
+// https://ngxtension.netlify.app/utilities/signals/create-notifier/
+export function createNotifier() {
+	const sourceSignal = signal(0);
+
+	return {
+		notify: () => {
+			sourceSignal.update((v) => (v >>> 0) + 1);
+		},
+		listen: sourceSignal.asReadonly(),
+	};
+}
--- src/app/http-resource.component.ts
+++ src/app/http-resource.component.ts
@@ -1,13 +1,20 @@
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, SlicePipe } from '@angular/common';
 import { httpResource } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Todo } from './todo.service';
+import { createNotifier } from './create-notifier';
 
 @Component({
   selector: 'app-http-resource',
-  imports: [JsonPipe],
+  imports: [JsonPipe, SlicePipe],
   template: `
-    <pre>{{todos.value() | json}}</pre>
+    @for (todo of todos.value() | slice:0:3; track $index) {
+        <pre>{{todo | json}}</pre>
+    }
+    <button (click)="makeTodoNotifier.notify()">"Create" a TODO w/POST</button>
+    @if(makeTodo.value()) {
+        <pre>{{makeTodo.value() | json}}</pre>
+    }
   `,
   styles: ``
 })
@@ -19,4 +26,13 @@ export class HttpResourceComponent {
 
     // Default GET
     todos = httpResource<Todo[]>(this.url, {defaultValue: []})
+
+
+    makeTodoNotifier = createNotifier();
+    // From what I can initially tell, this is how to make something like a POST be on demand?
+    // https://github.com/angular/angular/pull/59876/files#diff-a3ccb3778a1821be2d303f932f53036a5ff507cda412db0ed800fa55dba698bdR47
+    // `it('should not make backend requests if the request is undefined',`
+    makeTodo = httpResource<Todo>(() => (
+        this.makeTodoNotifier.listen() ? {url: this.url, method: 'POST', body: {title: 'test', completed: true, userId: 1}}: undefined)
+    )
 }
