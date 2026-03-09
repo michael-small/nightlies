06f57a4a9535d42ea042e9c68ed5663d6ea4c097
Michael Small <michaelsmalldev@gmail.com>
2026-03-09 17:06:29 -0500

    chore: add link to debounce PR + current nightly SHA

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,28 @@
 import { FormsModule } from '@angular/forms';
 import { Component, Resource, signal, VERSION } from '@angular/core';
 
-import { debounced } from '@angular/core'; // in the nightly as of 3/9/26
+import { debounced } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
+    <!-- Nightly as of 3/9/26: 22.0.0-next.1+sha-b918bed -->
     <pre>Version: {{version.full}}</pre>
+
+    <p>
+      <a 
+        href="https://github.com/angular/angular/pull/67044" 
+        target="_blank">feat(core): allow debouncing signals #67044
+      </a>
+    </p>
 
     <label>
       Source
       <input [(ngModel)]="source" />
     </label>
 
-    <p>Source: {{source()}}</p>
-    <p>Debounced: {{debounced.value()}}</p>
+    <p>source value: {{source()}}</p>
+    <p>debouncedRes (a resource) value: {{debouncedRes.value()}}</p>
   `,
   imports: [FormsModule]
 })
@@ -23,5 +31,5 @@ export class App {
 
   protected source = signal('value')
 
-  protected debounced: Resource<string> = debounced(this.source, 1000);
+  protected debouncedRes: Resource<string> = debounced(this.source, 1000);
 }
